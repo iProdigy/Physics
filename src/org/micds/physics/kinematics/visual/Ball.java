02331fb043c747c02b00ed7413577584dff7d92f
iProdigy <prodigycreep@gmail.com>
2016-09-26 11:04:43 -0500

    Construct some balls

--- src/org/micds/physics/kinematics/visual/Ball.java
+++ src/org/micds/physics/kinematics/visual/Ball.java
@@ -8,9 +8,21 @@ import org.micds.physics.kinematics.Entity;
 public class Ball extends Sphere {
 	private final Entity ent;
 
+	public Ball(final Entity entity) {
+		this(1.0, entity);
+	}
+
+	public Ball(final double[] position) {
+		this(new Entity(position));
+	}
+
 	public Ball(final double radius, final double[] position) {
+		this(radius, new Entity(position));
+	}
+
+	public Ball(final double radius, final Entity ent) {
 		super(radius);
-		this.ent = new Entity(position);
+		this.ent = ent;
 	}
 
 	// TODO: Bridge gap between physics entity and javafx sphere representation
